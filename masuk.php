4dc0e942267cb9b767ed5c284e96a49d43d1ef72
showcheap <show.cheap@gmail.com>
2013-03-22 07:16:04 +0700

    Redesign Login Page

--- masuk.php
+++ masuk.php
@@ -20,6 +20,55 @@ if(isset($_SESSION['level'])){
 }
 $nama=mysql_real_escape_string($_POST['nama']);
 $kunci=md5($_POST['kunci']);
+
+?>
+<html>
+<head>
+<title>Login | LIBSKA - SkanSoft&trade;</title>
+<link href="./tampilan/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+<script src='./tampilan/jq.js' type='text/javascript'></script>
+<script type='text/javascript'>
+    $(document).ready(function(){
+        $('#user').focus();
+    })
+</script>
+<script type='text/javascript'>
+    $(document).ready(function(){
+       
+            $.ajax({
+                url:'run.php',
+                success: function(data){
+                    //$("#res").html(data);
+                    
+                }
+            })
+       
+      });    
+</script>
+<style>
+.login {
+    width: 240px;
+    margin: 10% auto;
+    background-color: white;
+    border: 1px solid #999;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    -webkit-border-radius: 6px;
+    -moz-border-radius: 6px;
+    border-radius: 6px;
+    outline: 0;
+    -webkit-box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0 3px 7px rgba(0,0,0,0.3);
+    box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+    -webkit-background-clip: padding-box;
+    -moz-background-clip: padding-box;
+    background-clip: padding-box;
+}
+</style>
+</head>    
+<body>
+<?php
 if ($pencet != '' && $nama != '' && $kunci != ''){
     $s=mysql_query("SELECT * FROM tbl_pustakawan WHERE user='".$nama."' AND kunci='".$kunci."'");
     $c=mysql_num_rows($s);
@@ -37,57 +86,41 @@ if ($pencet != '' && $nama != '' && $kunci != ''){
       exit();
     }else{
         catat($nama,"Gagal Login");
-        echo "<script type='text/javascript'>alert('Kombinasi Salah !');</script>";
+        $script= "<script type='text/javascript'>";
+        $script.="$('document').ready(function(){";
+        $script.="$('#result').html('<p class=\'alert alert-error\'>Username dan Password tidak cocok !</p>');";
+        $script.="})";
+        $script.="</script>";
+        echo $script;
     }
 };
 ?>
-<html>
-<head>
-<title>Login | LIBSKA - SkanSoft&trade;</title>
-<link href="./tampilan/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-<script type='text/javascript'>
-    function about(){
-        alert("Libska Version <?php echo getVersion(); ?>\nPerpustakaan <?php echo get_sistem("nama"); ?>\n\nProgram By: Sucipto\nDedicated To: SMKN 1 Ngawi\nVisit: www.sucipto.net\n\nCopyright 2012 Alright Reserved")
-    }
-</script>
-<script src='./tampilan/jq.js' type='text/javascript'></script>
-<script type='text/javascript'>
-    $(document).ready(function(){
-       
-            $.ajax({
-                url:'run.php',
-                success: function(data){
-                    //$("#res").html(data);
-                    
-                }
-            })
-       
-      });    
-</script>
-</head>    
-<body>
-<div id='login' style="width: 300px; border: 1px solid; margin: 100px auto; padding: 10px;">
-
-        <fieldset>
-            <legend> <img src='tampilan/gambar/pinjem.png'> Login Petugas Perpustakaan</legend>
-            <form action='' method='POST'>
-        <table border='0' cellpadding='3' cellspacing='2' width='100%' style="margin-left:50px;">
-            <tr>
-                <td><input type='text' placeholder="Username" name='nama' style="height: 30px;"></td>
-            </tr>
-           
-            <tr>
-                <td><input type='password' placeholder="Kunci" name='kunci' style="height: 30px;"></td>
-            </tr>
-            
-            <tr>
-                <td valign='top' colspan='3'><input class='submit btn' type='submit' value='Masuk' name='tmbl'></td>
-            </tr>
-        </table>
-        </form>
-        </fieldset>
-<i style='font-size: 12px; font-style: italic; color: gray;'> Copyright &copy; <?php echo date('Y'); ?> | <b style='font-size: 11px;'>Libska Version: <?php echo getVersion();; ?></b> | <b onclick='about()' style='font-size: 12px;'>Tentang</b></i>
-</div>
+<div class="login" id="login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false" style="display: block;">
+                <div class="modal-header">
+                    <h3 id="myModalLabel">Login User</h3>
+                </div>
+                <form action='' method='POST'>
+                <div class="modal-body">
+                    <table>
+                    <tbody><tr>
+                            <td width="100%" id="tdUser">
+                                <input id="user" type="text" name="nama" style="height:40px; width: 100%;" placeholder="User ID" value="">
+                            </td>
+                    </tr>
+                    <tr>
+                            <td id="tdPass">
+                                <input type="password" id="pass" name="kunci" style="height:40px" placeholder="Password" value="">
+                            </td>
+                    </tr>
+                    </tbody>
+                    </table>
+                <div id="message"><!-- Result Message Here --></div>
+		</div>
+		<div id="result" style="margin: 5px;"><!-- Result --></div>
+                <div class="modal-footer" style="text-align: center;">
+                   <input class='submit btn btn-info span2' type='submit' value='Masuk' name='tmbl'>
+                </div>
+                </form>
+</div>    
 </body>    
 </html>
--- sistem/config.php
+++ sistem/config.php
@@ -4,6 +4,7 @@ File: config.php
 Fungsi: Koneksi ke Database
 Auth: ShowCheap
 */
+error_reporting('0');
 require 'fungsi.php';
 function sambung($db='libska', $host='localhost', $user='root', $pass='root'){
     @mysql_connect($host,$user,$pass) or die('<strong style="color: red;">Gagal Terhubung ke database '.mysql_error().'</strong>');
