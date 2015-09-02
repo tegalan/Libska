cfc8d36b944c8ad2b4b51194d35fc7716cb5e524
Sucipto <show.cheap@gmail.com>
2015-09-02 10:38:39 +0700

    Update config.php
    Update blank password

--- sistem/config.php
+++ sistem/config.php
@@ -6,7 +6,7 @@ Auth: ShowCheap
 */
 error_reporting('0');
 require 'fungsi.php';
-function sambung($db='libska', $host='localhost', $user='root', $pass='root'){
+function sambung($db='libska', $host='localhost', $user='root', $pass=''){
     @mysql_connect($host,$user,$pass) or die('<strong style="color: red;">Gagal Terhubung ke database '.mysql_error().'</strong>');
     @mysql_select_db($db) or die('<strong style="color: red;">Gagal Memilih database</strong>');;
 };
